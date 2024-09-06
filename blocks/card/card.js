e28d5e1c20db51a0b0592d809bac7e198b6d5e4f
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2024-09-05 23:04:41 -0400

    card

---
+++ blocks/card/card.js
@@ -0,0 +1,32 @@
+import { getRelativePath, getIndexData } from '../../scripts/utils.js';
+
+export function populateCard(container, cardInfo) {
+  const card = document.createElement('div');
+  card.className = 'card';
+  card.innerHTML = `
+        <a href="${cardInfo.path}">
+        <picture>
+            <source srcset="${cardInfo.image}" media="(min-width: 750px)">
+            <img src="${cardInfo.image}" alt="${cardInfo.title}">
+        </picture>
+        </a>
+        <div class="card-body">
+        <a href="${cardInfo.path}">
+            <h3>${cardInfo.title}</h3>
+        </a>
+            <p>${cardInfo.description}</p>
+        </div>
+    `;
+  container.replaceChildren(card);
+}
+
+export default async function decorate(block) {
+  const link = block.querySelector('a');
+  const path = link ? link.getAttribute('href') : block.textContent.trim();
+  const relPath = getRelativePath(path);
+  const indexData = await getIndexData();
+  const cardInfo = indexData.find((item) => item.path === relPath);
+  if (cardInfo) {
+    populateCard(block, cardInfo);
+  }
+}
