3cc88885e6fa27fbd895ee06df84ee3afd9af051
Kailas Nadh U <53608019+kailasnadh790@users.noreply.github.com>
2024-09-11 15:27:47 -0400

    Research efficacy (#132)
    * initial commit
    
    * Update research-filter.js
    
    * card
    
    * progress
    
    * progress
    
    * progress
    
    * progress
    
    * Update research-filter.css
    
    * Update research-filter.js
    
    * review
    
    * Update card.js
    
    * Update research-detail.js
    
    * efficacy badge
    
    * Update card.css
    
    * Delete flag-regular.svg
    
    * hero
    
    * Update errorpage.css
    
    * nav links
    
    * progress
    
    * complete
    
    * Update subnavigation.css
    
    * Update subnavigation.css
    
    * formatting

---
+++ blocks/subnavigation/subnavigation.js
@@ -0,0 +1,30 @@
+import { PRODUCT_COLORS } from '../../scripts/constants.js';
+
+export default async function decorate(block) {
+  const wrapper = block.querySelector('div');
+  const firstColumn = wrapper.querySelector('div');
+  firstColumn.classList.add('subnav-links');
+  const secondColumn = wrapper.querySelector('div:nth-child(2)');
+  if (secondColumn) {
+    secondColumn.classList.add('subnav-content');
+  }
+
+  const isResearch = block.classList.contains('research');
+  if (isResearch) {
+    const links = firstColumn.querySelectorAll('a');
+    links.forEach((link) => {
+      link.className = 'button secondary';
+      const href = link.getAttribute('href');
+      if (window.location.pathname === href) {
+        link.classList.add('active');
+      }
+      const lastPart = href.split('/').pop();
+      if (PRODUCT_COLORS[lastPart]) {
+        link.classList.add(`textcolor-${PRODUCT_COLORS[lastPart]}`);
+        if (secondColumn && window.location.pathname === href) {
+          secondColumn.classList.add(`${PRODUCT_COLORS[lastPart]}`);
+        }
+      }
+    });
+  }
+}
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,10 +22,22 @@ import {
 function buildHeroBlock(main) {
   const h1 = main.querySelector('h1');
   const picture = main.querySelector('picture');
+  const heroSubText = main.querySelector('p>em');
+  const heroContent = document.createElement('div');
+  heroContent.className = 'hero-content';
   // eslint-disable-next-line no-bitwise
   if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
     const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
+    heroContent.append(h1);
+    if (h1.compareDocumentPosition(heroSubText) && Node.DOCUMENT_POSITION_FOLLOWING) {
+      const h2 = document.createElement('h2');
+      h2.append(heroSubText.textContent);
+      heroSubText.remove();
+      heroContent.append(h2);
+      section.append(buildBlock('hero', { elems: [picture, heroContent] }));
+    } else {
+      section.append(buildBlock('hero', { elems: [picture, heroContent] }));
+    }
     main.prepend(section);
   }
 }
@@ -120,10 +132,11 @@ export function decorateButtons(element) {
       const up = a.parentElement;
       const twoup = a.parentElement.parentElement;
       if (!a.querySelector('img')) {
-        if (up.childNodes.length === 1 && (up.tagName === 'P' || up.tagName === 'DIV')) {
-          a.classList.add('button'); // default
-          up.classList.add('button-container');
-        }
+        /* Commenting out below code to prevent any links from being converted to buttons */
+        // if (up.childNodes.length === 1 && (up.tagName === 'P' || up.tagName === 'DIV')) {
+        //   a.classList.add('button'); // default
+        //   up.classList.add('button-container');
+        // }
         if (
           up.childNodes.length === 1
           && up.tagName === 'STRONG'
