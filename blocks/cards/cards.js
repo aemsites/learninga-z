112c5606384a817ace931bb576f1698eb5681214
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2024-09-24 16:42:38 -0400

    cards

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,12 +1,19 @@
 // These are the OOTB manual cards
-import { buildBlock } from '../../scripts/aem.js';
+import { buildBlock, decorateBlock, loadBlock } from '../../scripts/aem.js';
 
 export default function decorate(block) {
+  const title = block.querySelector('h4');
   const cardLinks = block.querySelectorAll('a');
   const cardsWrapper = document.createElement('div');
   cardsWrapper.className = 'cards-wrapper';
   cardLinks.forEach((cardLink) => {
-    cardsWrapper.append(buildBlock('card', cardLink));
-    cardLink.remove();
+    const card = buildBlock('card', [[cardLink]]);
+    cardsWrapper.append(card);
+    decorateBlock(card);
+    loadBlock(card);
   });
+  if (title) {
+    block.replaceChildren(title);
+  }
+  block.append(cardsWrapper);
 }
