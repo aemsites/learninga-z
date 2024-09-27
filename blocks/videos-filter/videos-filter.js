29a50f6f81ff7468cb625c5e3f673b894ff66e56
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2024-09-27 11:33:16 -0400

    Update videos-filter.js

--- blocks/videos-filter/videos-filter.js
+++ blocks/videos-filter/videos-filter.js
@@ -1,40 +1,49 @@
 /* eslint-disable max-len */
 import { getVideosIndexData } from '../../scripts/utils.js';
+import { populateCard } from '../card/card.js';
+import { loadCSS } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
   const videos = await getVideosIndexData();
-  console.log(videos);
+  // extracting unique category as an array from videos
+  const categories = new Map();
+  videos.forEach((video) => {
+    video.category.split(',').forEach((cat) => {
+      if (cat) {
+        const key = cat.toLowerCase().trim().replaceAll(' ', '-');
+        const value = cat.trim();
+        categories.set(key, value);
+      }
+    });
+  });
   const form = document.createElement('form');
   form.setAttribute('class', 'video-filter-form');
   form.setAttribute('method', 'get');
   form.innerHTML = `
       <div class="select-position">
       <select id="category" name="category" onchange="this.form.submit()">
-        <option value="ShowAll" selected="selected">Show All</option>
-        <option value="learninga-z">Learning A-Z</option>
-        <option value="foundationsa-z">Foundations A-Z</option>
-        <option value="razplus">Raz-Plus</option>
-        <option value="razplus-connected-classroom">Raz-Plus Connected Classroom</option>
-        <option value="readinga-z">Reading A-Z</option>
-        <option value="raz-kids">Raz-Kids</option>
-        <option value="sciencea-z">Science A-Z</option>
-        <option value="writinga-z">Writing A-Z</option>
-        <option value="vocabularya-z">Vocabulary A-Z</option>
-        <option value="razplus-ell">Raz-Plus ELL</option>
-        <option value="funding">Funding</option>
-        <option value="testimonials">Testimonials</option>
-        <option value="professional-learning">Professional Learning</option>
+        <option value="show-all" selected="selected">Show All</option>
+        ${[...categories].map(([key, value]) => `<option value="${key}">${value}</option>`).join('')}
       </select>
       </div>
     `;
 
   const urlParams = new URLSearchParams(window.location.search);
   const category = urlParams.get('category');
-  if (category) {
-    form.querySelector(`option[value="${category}"]`).selected = true;
-    const filteredVideos = videos.filter((video) => video.category.toLowerCase().includes(category));
-    console.log('filteredVideos', filteredVideos);
-  }
-
   block.append(form);
+  let filteredVideos = [];
+  if (category && category !== 'show-all') {
+    if (form.querySelector(`option[value="${category}"]`)) {
+      form.querySelector(`option[value="${category}"]`).selected = true;
+    }
+    filteredVideos = videos.filter((video) => video.category.toLowerCase().trim().replaceAll(' ', '-').includes(category));
+  } else {
+    filteredVideos = videos;
+  }
+  const div = document.createElement('div');
+  block.append(div);
+  div.className = 'cards-wrapper';
+  filteredVideos.forEach((video) => {
+    populateCard(div, video);
+  });
 }
