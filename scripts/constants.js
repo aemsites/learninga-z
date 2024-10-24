c75d8e8730ae8e533b879c1ff8046ee86cf39043
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2024-10-24 17:26:16 -0400

    fix for prof services

--- scripts/constants.js
+++ scripts/constants.js
@@ -3,4 +3,4 @@ export const PRODUCT_COLORS = {
   'raz-kids': 'dark-blue', 'raz-plus': 'berry', 'foundationsa-z': 'navy-blue', 'writinga-z': 'orange', 'vocabularya-z': 'purple', 'sciencea-z': 'green', 'raz-plus-ell': 'berry', 'research-and-efficacy': 'royal-blue',
 };
 
-export const PRODUCT_NAMES = ['Reading A-Z', 'Raz-Kids', 'Raz-Plus', 'Vocabulary A-Z', 'Science A-Z', 'Writing A-Z', 'Raz-Plus ELL'];
+export const PRODUCT_NAMES = ['Reading A-Z', 'Raz-Kids', 'Raz-Plus', 'Vocabulary A-Z', 'Science A-Z', 'Writing A-Z', 'Raz-Plus ELL', 'General'];
