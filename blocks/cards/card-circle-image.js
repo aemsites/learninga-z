c0d18072bd192655de7143075f83ed20ca8abb2f
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2024-10-31 14:48:25 -0400

    progress

--- blocks/cards/card-circle-image.js
+++ blocks/cards/card-circle-image.js
@@ -2,33 +2,36 @@
 import {
   getRelativePath,
 } from '../../scripts/utils.js';
-import { createOptimizedPicture } from '../../scripts/aem.js';
+import { createOptimizedPicture, decorateIcons } from '../../scripts/aem.js';
 
 export async function getCircleCardsArray(block, indexData, isDescription) {
+  decorateIcons(block);
   const cards = [];
   [...block.children].forEach((row) => {
     let card;
-    let img;
+    let img = '';
     [...row.children].forEach((col) => {
-      const pic = col.querySelector('picture');
+      const pic = col.querySelector('img');
+      console.log(col, pic);
       const a = col.querySelector('a');
       if (pic) {
-        img = pic.querySelector('img')?.getAttribute('src');
+        img = pic.getAttribute('src');
       }
       if (a) {
         const path = a.getAttribute('href');
         const relPath = getRelativePath(path);
         card = indexData.find((item) => item.path === relPath);
+        card.image = img;
+        card.isDescription = isDescription;
+        cards.push(card);
       }
     });
-    card.image = img;
-    card.isDescription = isDescription;
-    cards.push(card);
   });
   return cards;
 }
 
 export function populateCircleImageCard(wrapper, cardInfo) {
+  const theme = document.querySelector('meta[name="theme"]').getAttribute('content');
   const card = document.createElement('div');
   card.className = 'card';
   card.innerHTML = `
@@ -39,10 +42,11 @@ export function populateCircleImageCard(wrapper, cardInfo) {
                   </div>
           <div class="card-body">
                 <a href="${cardInfo.path}">
-                    <h3>${cardInfo.title}</h3>
+                    <h3 style=color:var(--${theme})>${cardInfo.title}</h3>
                 </a>
                 ${cardInfo.isDescription ? ` <a href="${cardInfo.path}"><p>${cardInfo.description}</p></a>` : ''}
           </div>
       `;
+  card.querySelector('img').setAttribute('style', `background-color: var(--${theme});`);
   wrapper.append(card);
 }
