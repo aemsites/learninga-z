84fb0c49ec6b3b1408213edde5c282b1a07c1f74
Charity Helms <chelms@adobe.com>
2024-11-12 15:36:43 -0500

    adding h2 element for best practices

--- templates/blog-author/blog-author.js
+++ templates/blog-author/blog-author.js
@@ -4,6 +4,9 @@ export default function buildAuthorPage(main) {
   if (main) {
     const authorCards = document.createElement('div');
     authorCards.classList.add('author-cards');
+    const h2 = document.createElement('h2');
+    h2.innerText = 'Related Articles';
+    authorCards.append(h2);
     authorCards.append(buildBlock('author-filter', ''));
     main.append(authorCards);
   }
