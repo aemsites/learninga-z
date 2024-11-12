0478e77712b195b5d4fa3f671dd4c0c565fe4036
Charity Helms <chelms@adobe.com>
2024-11-12 15:16:53 -0500

    author page blog cards

---
+++ blocks/author-filter/author-filter.js
@@ -0,0 +1,40 @@
+/* blog-article specific JavaScript goes here */
+import { getBlogsIndexData } from '../../scripts/utils.js';
+import { renderCardList } from '../cards/cards.js';
+import { getMetadata, loadCSS } from '../../scripts/aem.js';
+
+//  sets the "author" value from current page's metadata to a variable.
+const blogs = await getBlogsIndexData();
+// extracting unique author as an array from blogs
+const authors = new Map();
+blogs.forEach((blog) => {
+  blog.author.split(',').forEach((auth) => {
+    if (auth) {
+      const key = auth.toLowerCase().trim().replaceAll(' ', '-');
+      const value = auth.trim();
+      authors.set(key, value);
+    }
+  });
+});
+
+export async function getBlogsByAuthor() {
+  const authName = getMetadata('author');
+  console.log('authors', authors);
+  const filteredBlogs = blogs.filter((blog) => blog.author.split(',').map((auth) => auth.toLowerCase().trim().replaceAll(' ', '-')).includes(authName.toLowerCase().trim().replaceAll(' ', '-'))
+      && blog.template === 'blog-article');
+  if (filteredBlogs.length > 0) {
+    return filteredBlogs;
+  }
+  console.error('Author not found or does not match');
+  return [];
+}
+
+export default async function decorate(block) {
+  await loadCSS(`${window.hlx.codeBasePath}/blocks/cards/cards.css`);
+  const authorBlogs = await getBlogsByAuthor();
+  console.log('author Blogs', authorBlogs);
+  const div = document.createElement('div');
+  block.append(div);
+  div.className = 'cards';
+  renderCardList(div, authorBlogs, 100, 'card');
+}
--- templates/blog-author/blog-author.js
+++ templates/blog-author/blog-author.js
@@ -1,1 +1,10 @@
-/* blog-article specific JavaScript goes here */
+import { buildBlock } from '../../scripts/aem.js';
+
+export default function buildAuthorPage(main) {
+  if (main) {
+    const authorCards = document.createElement('div');
+    authorCards.classList.add('author-cards');
+    authorCards.append(buildBlock('author-filter', ''));
+    main.append(authorCards);
+  }
+}
