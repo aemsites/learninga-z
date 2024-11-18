330bae0cb7bac9a9f35c70be6959d1bc754e2b25
Charity Helms <chelms@adobe.com>
2024-11-18 17:17:53 -0500

    translucent bg added, other color fixes

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 export const PRODUCT_COLORS = {
-  'raz-kids': 'dark-blue', 'raz-plus': 'berry', 'foundationsa-z': 'navy-blue', 'writinga-z': 'orange', 'vocabularya-z': 'purple', 'sciencea-z': 'green', 'raz-plus-ell': 'berry', 'research-and-efficacy': 'royal-blue',
+  'raz-kids': 'dark-blue', 'raz-plus': 'berry', 'foundationsa-z': 'navy-blue', 'writinga-z': 'orange', 'vocabularya-z': 'purple', 'sciencea-z': 'green', 'raz-plus-ell': 'berry', 'research-and-efficacy': 'navy-blue',
 };
 
 export const PRODUCT_NAMES = ['Reading A-Z', 'Raz-Kids', 'Raz-Plus', 'Vocabulary A-Z', 'Science A-Z', 'Writing A-Z', 'Raz-Plus ELL', 'General'];
