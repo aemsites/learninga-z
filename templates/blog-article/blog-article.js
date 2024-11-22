c8925d3986e9f2464c396f112871f7cfca3f82b0
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2024-11-22 10:12:12 -0500

    Update blog-article.js

--- templates/blog-article/blog-article.js
+++ templates/blog-article/blog-article.js
@@ -1,6 +1,10 @@
-import { buildBlock, getMetadata } from '../aem.js';
+import { buildBlock, getMetadata } from '../../scripts/aem.js';
 
 export default async function decorate(main) {
   const picture = main.querySelector('picture');
   const author = getMetadata('author');
+  const authorInfo = buildBlock('author-info', [
+    [`<p>${author}</p>`],
+  ]);
+  picture.parentNode.insertBefore(authorInfo, picture);
 }
