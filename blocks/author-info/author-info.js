de44c9fd555fa4db5d2f71384b78ac24207051b9
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2024-11-22 11:35:24 -0500

    author autoblocking

--- blocks/author-info/author-info.js
+++ blocks/author-info/author-info.js
@@ -1,18 +1,34 @@
 /* eslint-disable max-len */
 import { getAuthors } from '../../scripts/utils.js';
+import { createOptimizedPicture } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
   const authorObjects = await getAuthors();
-  const authors = block.querySelector(':scope > p');
-  const authorList = authors[0].textContent.split(',');
-  block.innerHTML = '<p>By ';
+  const authors = block.querySelector('p').innerText;
+  const authorList = authors.split(',');
+  block.innerHTML = '';
   authorList.forEach((authorItem) => {
     const authorData = authorObjects.find((authorObject) => authorObject.author.toLowerCase() === authorItem.toLowerCase().trim());
     if (authorData) {
-      block.innerHTML += `<a href="${authorData.path}">${authorData.author}</a> ${authorData['author-title']}`;
+      if (authorList.length === 1 && authorData.image) {
+        block.innerHTML += createOptimizedPicture(authorData.image, authorData.author).outerHTML;
+      }
+      if (authorList.indexOf(authorItem) === 0) {
+        block.innerHTML += 'By ';
+      }
+      block.innerHTML += `<a href="${authorData.path}">${authorData.author}</a>${authorData.authorTitle?.length ? `, ${authorData.authorTitle}` : ''}`;
       if (authorList.indexOf(authorItem) < authorList.length - 1) {
         block.innerHTML += '; ';
       }
     }
   });
+
+  const children = Array.from(block.childNodes);
+  const p = document.createElement('p');
+  children.forEach((child) => {
+    if (child.tagName !== 'PICTURE') {
+      p.appendChild(child);
+    }
+  });
+  block.appendChild(p);
 }
--- templates/blog-article/blog-article.js
+++ templates/blog-article/blog-article.js
@@ -1,10 +1,22 @@
+/* eslint-disable max-len */
 import { buildBlock, getMetadata } from '../../scripts/aem.js';
+import { div } from '../../scripts/dom-helpers.js';
 
 export default async function decorate(main) {
-  const picture = main.querySelector('picture');
   const author = getMetadata('author');
   const authorInfo = buildBlock('author-info', [
     [`<p>${author}</p>`],
   ]);
-  picture.parentNode.insertBefore(authorInfo, picture);
+
+  const h1 = main.querySelector('h1');
+  let subHeading = main.querySelector('p>strong');
+  if (!subHeading) {
+    subHeading = main.querySelector('strong>p');
+  }
+  let headingSection = div(h1);
+  if (subHeading && subHeading.innerText === subHeading.closest('p').innerText) {
+    headingSection = div(h1, subHeading);
+  }
+  main.prepend(div(authorInfo));
+  main.prepend(headingSection);
 }
