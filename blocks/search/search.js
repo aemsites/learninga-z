4fdd6f0c49f871ab5a1055055cc1e3af5e3d2568
dave-fink <david@keypixel.com>
2024-11-26 16:24:11 -0500

    Latest

--- blocks/search/search.js
+++ blocks/search/search.js
@@ -2,7 +2,7 @@
 import { getSiteIndexData } from '../../scripts/utils.js';
 import { loadCSS } from '../../scripts/aem.js';
 import { renderCardList } from '../cards/cards.js';
-import { h1 } from '../../scripts/dom-helpers.js';
+import { div, form } from '../../scripts/dom-helpers.js';
 
 export default async function decorate(block) {
   await loadCSS(`${window.hlx.codeBasePath}/blocks/cards/cards.css`);
@@ -11,6 +11,8 @@ export default async function decorate(block) {
   // Create a URL object
   const searchParams = new URLSearchParams(window.location.search);
   const searchTerm = searchParams.get('search');
+  const filter = searchParams.get('filter');
+  console.log(filter);
 
   function filterSiteIndex(index, keyword) {
     return index.filter((item) => Object.values(item).some((value) => value && value.toString().toLowerCase().includes(keyword.toLowerCase()),));
@@ -18,42 +20,32 @@ export default async function decorate(block) {
 
   const filteredResults = filterSiteIndex(siteIndex, searchTerm);
 
-  const $h1 = h1(`Searching for "${searchTerm}"`);
-  block.append($h1);
+  const $form = form({ class: 'search', method: 'get' });
+  $form.innerHTML = `
+    <h2>Searching for "${searchTerm}"</h2>
+    <div class="right">
+      <h3>Filter by:</h3>
+      <select id="filter" name="filter" onchange="window.location.hash = 'page=1';this.form.submit();">
+          <option value="">Show All Content</option>
+          <option value="general">General</option>
+          <option value="news">Company News</option>
+          <option value="products">Products</option>
+          <option value="videos">Videos</option>
+          <option value="breakroom">Breakroom Post</option>
+          <option value="funding-and-grants">Funding and Grants</option>
+          <option value="download-library">Download Library</option>
+          <option value="professional-development">Professional Development</option>
+          <option value="research">Research</option>            
+      </select>
+    </div>
+  `;
 
-  const form = document.createElement('form');
-  form.setAttribute('class', 'news-filter-form');
-  form.setAttribute('method', 'get');
-  form.innerHTML = `
-        <div class="select-position">
-        <select id="sortorder" name="sortorder" onchange="window.location.hash = 'page=1';this.form.submit();">
-            <option value="NewestFirst" selected="selected">Sort By: Newest First</option>
-            <option value="OldestFirst">Sort By: Oldest First</option>
-            <option value="AlphaAZ">Sort By: A - Z</option>
-            <option value="AlphaZA">Sort By: Z - A</option>
-        </select>
-        </div>
-    `;
+  const $search = div({ class: 'search-cards' });
+  block.append($form, $search);
 
-  const urlParams = new URLSearchParams(window.location.search);
-  const sortOrder = urlParams.get('sortorder');
-  block.append(form);
-  if (sortOrder === 'OldestFirst') {
-    filteredResults.reverse();
-    form.querySelector('option[value="OldestFirst"]').selected = true;
-  } else if (sortOrder === 'AlphaAZ') {
-    filteredResults.sort((a, b) => a.title.localeCompare(b.title));
-    form.querySelector('option[value="AlphaAZ"]').selected = true;
-  } else if (sortOrder === 'AlphaZA') {
-    filteredResults.sort((a, b) => b.title.localeCompare(a.title));
-    form.querySelector('option[value="AlphaZA"]').selected = true;
-  }
-  const div = document.createElement('div');
-  block.append(div);
-  div.className = 'news-cards';
-  renderCardList(div, filteredResults, 10, 'news');
+  renderCardList($search, filteredResults, 10, 'news');
 
   window.addEventListener('hashchange', async () => {
-    renderCardList(div, filteredResults, 10, 'news');
+    renderCardList($search, filteredResults, 10, 'news');
   });
 }
