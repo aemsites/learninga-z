aa99634a8bc26b6ee6842aac0269b384669b0388
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2024-12-05 13:01:09 -0500

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -78,55 +78,95 @@ async function enableIntercom() {
 }
 enableIntercom();
 
-// load css https://cdn.popupsmart.com/accounts/6030/21310/13/main.css for popupsmart
-// async function enablePopupSmart() {
-//   setTimeout(async () => {
-//     await loadScript('https://cdn.popupsmart.com/accounts/6030/21310/13/main.js', {
-//       type: 'text/javascript',
-//       async: true,
-//       defer: true,
-//     });
-//     const popupSmartContainer = document.querySelector('#popupsmart-container-21310');
-//     if (popupSmartContainer) {
-//       let shadowRoot;
-//       if (popupSmartContainer.shadowRoot) {
-//         shadowRoot = popupSmartContainer.shadowRoot;
-//       } else {
-//         shadowRoot = popupSmartContainer.attachShadow({ mode: 'open' });
-//       }
-//       const popupSmartCss = document.createElement('link');
-//       popupSmartCss.rel = 'stylesheet';
-//       popupSmartCss.href = 'https://cdn.popupsmart.com/accounts/6030/21310/13/main.css';
-//       shadowRoot.appendChild(popupSmartCss);
+async function enablePopupSmart() {
+  setTimeout(async () => {
+    await loadScript('https://cdn.popupsmart.com/accounts/6030/21310/13/main.js', {
+      type: 'text/javascript',
+      async: true,
+      defer: true,
+    });
+    const popupSmartContainer = document.querySelector('#popupsmart-container-21310');
+    if (popupSmartContainer) {
+      let shadowRoot;
+      if (popupSmartContainer.shadowRoot) {
+        shadowRoot = popupSmartContainer.shadowRoot;
+      } else {
+        shadowRoot = popupSmartContainer.attachShadow({ mode: 'open' });
+      }
+      const popupSmartCss = document.createElement('link');
+      popupSmartCss.rel = 'stylesheet';
+      popupSmartCss.href = 'https://cdn.popupsmart.com/accounts/6030/21310/13/main.css';
+      shadowRoot.appendChild(popupSmartCss);
+    }
+  }, 1000);
+}
+
+function enableGoogleTagManager() {
+  // Create an instance of the Web Worker
+  const gtmWorker = new Worker(`${window.hlx.codeBasePath}/scripts/googletagmanager-worker.js`);
+
+  // Send a message to the Web Worker to load the GTM script
+  gtmWorker.postMessage('loadGTM');
+
+  // Optional: Listen for messages from the Web Worker
+  gtmWorker.onmessage = function (event) {
+    if (event.data.error) {
+      console.error('Error in Web Worker:', event.data.error);
+    } else {
+      // Create a script element and inject the GTM script into the DOM
+      const gtmScript = document.createElement('script');
+      gtmScript.type = 'text/javascript';
+      gtmScript.innerHTML = event.data;
+      const l = 'dataLayer';
+      window[l] = window[l] || [];
+      window[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js',
+      });
+      const f = document.getElementsByTagName('script')[0];
+      f.parentNode.insertBefore(gtmScript, f);
+      const noscriptElement = document.createElement('noscript');
+      const iframeElement = document.createElement('iframe');
+      iframeElement.src = 'https://www.googletagmanager.com/ns.html?id=GTM-NXTTWP';
+      iframeElement.height = '0';
+      iframeElement.width = '0';
+      iframeElement.style.display = 'none';
+      iframeElement.style.visibility = 'hidden';
+      noscriptElement.appendChild(iframeElement);
+
+      document.body.insertAdjacentElement('afterbegin', noscriptElement);
+      enablePopupSmart();
+    }
+  };
+
+  // Optional: Handle errors from the Web Worker
+  gtmWorker.onerror = function (error) {
+    console.error('Error in Web Worker:', error);
+  };
+}
+enableGoogleTagManager();
+
+// function loadGTM() {
+//   const gtagWorker = new Worker(`${window.hlx.codeBasePath}/scripts/gtag-worker.js`);
+//   gtagWorker.postMessage('loadGtag');
+//   gtagWorker.onmessage = function (event) {
+//     if (event.data.error) {
+//       console.error('Error in Web Worker:', event.data.error);
+//     } else {
+//       const tag = document.createElement('script');
+//       tag.type = 'text/javascript';
+//       tag.innerHTML = event.data;
+//       tag.async = true;
+//       tag.id = 'google-gtagjs-js';
+//       document.querySelector('head').append(tag);
+//       const script = document.createElement('script');
+//       script.type = 'text/javascript';
+//       script.id = 'google_gtagjs-js-after';
+//       script.innerHTML = GTM_SCRIPT;
+//       tag.insertAdjacentElement('afterend', script);
 //     }
-//   }, 1000);
+//   };
 // }
-
-// Google Tag Manager script embed
-// async function enableGoogleTagManager() {
-//   const gtmScript = document.createElement('script');
-//   gtmScript.type = 'text/javascript';
-//   gtmScript.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-//   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-//   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-//   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-//   })(window,document,'script','dataLayer','GTM-NXTTWP');`;
-//   gtmScript.async = true;
-
-//   const noscriptElement = document.createElement('noscript');
-//   const iframeElement = document.createElement('iframe');
-//   iframeElement.src = 'https://www.googletagmanager.com/ns.html?id=GTM-NXTTWP';
-//   iframeElement.height = '0';
-//   iframeElement.width = '0';
-//   iframeElement.style.display = 'none';
-//   iframeElement.style.visibility = 'hidden';
-//   noscriptElement.appendChild(iframeElement);
-
-//   document.head.appendChild(gtmScript);
-//   document.body.insertAdjacentElement('afterbegin', noscriptElement);
-//   enablePopupSmart();
-// }
-// enableGoogleTagManager();
 
 async function enablePardot() {
   const pardotScript = document.createElement('script');
