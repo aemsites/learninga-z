d5f9e497714240c4c4adbbc7ad96d5d9d0a6c6ae
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2024-12-06 16:08:59 -0500

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -176,45 +176,6 @@ function buildHeroBlock(main) {
   }
 }
 
-/**
- * create an element.
- * @param {string} tagName the tag for the element
- * @param {object} props properties to apply
- * @param {string|Element} html content to add
- * @returns the element
- */
-export function createElement(tagName, props, html) {
-  const elem = document.createElement(tagName);
-  if (props) {
-    Object.keys(props).forEach((propName) => {
-      const val = props[propName];
-      if (propName === 'class') {
-        const classesArr = (typeof val === 'string') ? [val] : val;
-        elem.classList.add(...classesArr);
-      } else {
-        elem.setAttribute(propName, val);
-      }
-    });
-  }
-
-  if (html) {
-    const appendEl = (el) => {
-      if (el instanceof HTMLElement || el instanceof SVGElement) {
-        elem.append(el);
-      } else {
-        elem.insertAdjacentHTML('beforeend', el);
-      }
-    };
-
-    if (Array.isArray(html)) {
-      html.forEach(appendEl);
-    } else {
-      appendEl(html);
-    }
-  }
-
-  return elem;
-}
 
 /**
  * load fonts.css and set a session storage flag
