572e14c4c1986394142b3eb2a611b1a16764a937
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2024-12-06 16:40:27 -0500

    Update accessibility-mode.js

--- tools/sidekick/plugins/accessibility-mode/accessibility-mode.js
+++ tools/sidekick/plugins/accessibility-mode/accessibility-mode.js
@@ -2,7 +2,7 @@
 import { Sa11y, Lang } from './lib/sa11y.min.js';
 import Sa11yLangEn from './lib/sa11y.lang.en.js';
 import { loadCSS } from '../../../../scripts/aem.js';
-import { createElement } from '../../../../scripts/scripts.js'; // eslint-disable-line import/no-cycle
+import { domEl } from '../../../../scripts/dom-helpers.js'; // eslint-disable-line import/no-cycle
 import customChecks from './custom-checks/custom-checks.js';
 
 let initializedCounter = 0;
@@ -20,17 +20,17 @@ const sa11yElements = [
 const isPanelOpened = () => localStorage.getItem('sa11y-remember-panel') === 'Opened';
 
 const createDialog = () => {
-  const dialog = createElement('div', { id: 'hlx-a11y-mode-dialog' }, [
-    createElement('div', { class: 'hlx-a11y-mode-dialog-container' }, [
-      createElement('h4', { class: 'hlx-a11y-mode-dialog-title' }, 'Welcome to Accessibility Mode!'),
-      createElement('p', {}, 'Accessibility Mode is a tool that helps you to identify accessibility issues on your page. It scans the page from a content perspective and reports on things like missing alt text, missing headings, etc.'),
-      createElement('p', {}, 'Accessibility Mode is not a replacement for a full accessibility audit and does not guarantee compliance with a certain accessibility standard. It is a tool that helps you to identify and fix issues that can be fixed by content authoring.'),
-      createElement('p', {}, 'The page outline provides a hierarchical view of the page content. It is a great way to understand the structure of the page and to identify any issues with the page structure.'),
-      createElement('p', {}, 'The readability score provides an indication of how easy it is to read the content on the pag, and it is based on the Flesch Reading Ease test.'),
-      createElement('p', {}, 'Custom checks for EDS blocks can be added on a per project basis.'),
-      createElement('sup', {}, 'Note: This tool is extended from the open source project <a href="https://sa11y.netlify.app/overview/" target="_blank">Sa11y</a>.'),
-      createElement('div', { class: 'hlx-a11y-mode-dialog-actions' }, [
-        createElement('button', { class: 'hlx-a11y-mode-dialog-button' }, 'Don’t Show Again'),
+  const dialog = domEl('div', { id: 'hlx-a11y-mode-dialog' }, [
+    domEl('div', { class: 'hlx-a11y-mode-dialog-container' }, [
+      domEl('h4', { class: 'hlx-a11y-mode-dialog-title' }, 'Welcome to Accessibility Mode!'),
+      domEl('p', {}, 'Accessibility Mode is a tool that helps you to identify accessibility issues on your page. It scans the page from a content perspective and reports on things like missing alt text, missing headings, etc.'),
+      domEl('p', {}, 'Accessibility Mode is not a replacement for a full accessibility audit and does not guarantee compliance with a certain accessibility standard. It is a tool that helps you to identify and fix issues that can be fixed by content authoring.'),
+      domEl('p', {}, 'The page outline provides a hierarchical view of the page content. It is a great way to understand the structure of the page and to identify any issues with the page structure.'),
+      domEl('p', {}, 'The readability score provides an indication of how easy it is to read the content on the pag, and it is based on the Flesch Reading Ease test.'),
+      domEl('p', {}, 'Custom checks for EDS blocks can be added on a per project basis.'),
+      domEl('sup', {}, 'Note: This tool is extended from the open source project <a href="https://sa11y.netlify.app/overview/" target="_blank">Sa11y</a>.'),
+      domEl('div', { class: 'hlx-a11y-mode-dialog-actions' }, [
+        domEl('button', { class: 'hlx-a11y-mode-dialog-button' }, 'Don’t Show Again'),
       ]),
     ]),
   ]);
